b8cd286735891ace660ba66e419d21f2114d9e1c
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-27 09:41:13 -0300

    ok

--- salvar.php
+++ salvar.php
@@ -2,3 +2,29 @@
 
 //var_dump($_POST);
 $_SESSION['cadastroPessoas'][] = $POST;
+
+if (isset($_POST['cadastrar'])) {
+    if (isset($_POST['first_name'])):
+        $firstName = $_POST['first_name'];
+    endif;
+
+    if (isset($_POST['last_name'])):
+        $lastName = $_POST['last_name'];
+    endif;
+
+    if (isset($_POST['username'])):
+        $username = $_POST['username'];
+    endif;
+
+    if (isset($_POST['estado'])):
+        $estado = $_POST['estado'];
+    endif;
+
+    if (isset($_POST['cidade'])):
+        $cidade = $_POST['cidade'];
+    endif;
+
+    if (isset($_POST['cep'])):
+        $cep = $_POST['cep'];
+    endif;
+}
