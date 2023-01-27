1a4d744e07ade991f832fe5d71355825951471c8
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-27 09:52:24 -0300

    ok

--- salvar.php
+++ salvar.php
@@ -27,4 +27,22 @@ if (isset($_POST['cadastrar'])) {
     if (isset($_POST['cep'])):
         $cep = $_POST['cep'];
     endif;
+} else {
+    $stmt = $conn->prepare("INSERT INTO usuarios (first_name, last_name, username, estado, cidade, cep)
+    VALUES(:first_name, :last_name, :username, :estado, :cidade, :cep)");
+}
+
+$stmt->bindParam(':first_name', $firstName, PDO::PARAM_STR);
+$stmt->bindParam(':last_name', $lastName, PDO::PARAM_STR);
+$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+$stmt->bindParam(':estado', $estado, PDO::PARAM_STR);
+$stmt->bindParam(':cidde', $cidade, PDO::PARAM_STR);
+$stmt->bindParam(':cep', $cep, PDO::PARAM_STR);
+
+$stmt->execute();
+
+if ($stmt->rowCount() > 0) {
+    echo 'Usuário Cadastrado com Sucesso';
+} else {
+    echo 'Usuário  não foi cadastrado ';
 }
