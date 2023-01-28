21a2091518e0b0d7702429680e3b57228c5c600e
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-27 21:46:25 -0300

    ok

--- PDO_BEGIN_TRANSACTION.php
+++ PDO_BEGIN_TRANSACTION.php
@@ -12,12 +12,9 @@ try {
   // begin the transaction
   $conn->beginTransaction();
   // our SQL statements
-  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES ('John', 'Doe', 'john@example.com')");
-  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES ('Mary', 'Moe', 'mary@example.com')");
-  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES ('Julie', 'Dooley', 'julie@example.com')");
+  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')");
+  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) VALUES ('Mary', 'Moe', 'mary@example.com')");
+  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) VALUES ('Julie', 'Dooley', 'julie@example.com')");
 
   // commit the transaction
   $conn->commit();
