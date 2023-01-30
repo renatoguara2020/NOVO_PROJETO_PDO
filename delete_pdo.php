34220af0fbdeac63e244bf1c3e9725fd36ef8d73
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-30 16:58:17 -0300

    OK

--- delete_pdo.php
+++ delete_pdo.php
@@ -5,18 +5,20 @@ $id = $_GET['id'];
 
 try {
   $conn = new PDO('mysql:host=localhost;dbname=meuBancoDeDados', $username, $password);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE_EXCEPTION, PDO::ERRMODE_EXCEPTION );
+  $pdo->setAttribute(PDO::ATTR_ERRMODE_WARNING, PDO::ERRMODE_WARNING);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
   $stmt = $conn->prepare('DELETE FROM minhaTabela WHERE id = :id');
   $stmt->bindParam(':id', $id);
-  $stmt->execute([':id' => $id]);
-  $stmt->execute(array(':id' => $id));
+  $stmt->execute([':id' => $id]); // IGUAL MESMO VALOR $stmt->execute(array(':id' => $id));
 
   if($stmt->rowCount() > 0){
 
     echo 'Usuário cadastrado com sucesso';
   }
 } catch(PDOException $e) {
-  echo 'Error: ' . $e->getMessage();
+
+  echo 'Error Throwable: ' . $e->getMessage(). ' '. $e->getTraceAsString();
 }
 ?>
