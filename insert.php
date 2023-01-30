d0bb2c2ee0e74775c63d2838cdc0391e0567af98
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-30 12:39:30 -0300

    OK

--- insert.php
+++ insert.php
@@ -4,6 +4,7 @@ $id = $_GET['id'];
 try {
   $pdo = new PDO('mysql:host=localhost;dbname=meuBancoDeDados', $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
   $stmt = $pdo->prepare('INSERT INTO minhaTabela (nome) VALUES(:nome)');
   $stmt->execute(array(
